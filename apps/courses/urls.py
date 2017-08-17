bbbaefb7c33baf8e5a3f54c2e813d32744d0ba6f
grantwbrooks <grantwbrooks@gmail.com>
2017-08-16 23:22:45 -0400

    udates

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^courses$', views.index, name='index'),
     url(r'^courses/add$', views.add, name='add'),
-    url(r'^/courses/destroy(?P<id>\d+)$', views.destroy, name='destroy'),
-    url(r'^/courses/confirm(?P<id>\d+)$', views.confirm, name='confirm'),
+    url(r'^courses/destroy(?P<id>\d+)$', views.destroy, name='destroy'),
+    url(r'^courses/confirm(?P<id>\d+)$', views.confirm, name='confirm'),
 ]
